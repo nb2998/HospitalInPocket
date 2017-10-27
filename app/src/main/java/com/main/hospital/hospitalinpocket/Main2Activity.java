1ab3d09a21943b2a323345dccdf21b67e24a9e91
meghna2712 <meghnagupta2712@gmail.com>
2017-10-28 01:33:42 +0530

    All activities added

---
+++ app/src/main/java/com/main/hospital/hospitalinpocket/Main2Activity.java
@@ -0,0 +1,41 @@
+package com.main.hospital.hospitalinpocket;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+     EditText et1;
+    TextView  t1;
+    Button b1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        et1 = (EditText)findViewById(R.id.editText1);
+        t1 = (TextView)findViewById(R.id.textView1);
+        b1 = (Button)findViewById(R.id.button1);
+
+        b1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+
+                Intent i= new Intent(Main2Activity.this, Main4Activity.class);
+
+                startActivity(i);
+            }
+
+
+        });
+    }
+}
