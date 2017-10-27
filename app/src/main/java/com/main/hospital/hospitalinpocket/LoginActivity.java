a40ddbe2f280394e4d9ac3f8331653d55e202325
Nishtha Bhatia <nbhatia329@gmail.com>
2017-10-28 04:57:42 +0530

    intents fired

--- app/src/main/java/com/main/hospital/hospitalinpocket/LoginActivity.java
+++ app/src/main/java/com/main/hospital/hospitalinpocket/LoginActivity.java
@@ -1,13 +1,55 @@
 package com.main.hospital.hospitalinpocket;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_signup,btn_login;
+    String loginBy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_signup = (Button) findViewById(R.id.btn_signup);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        Intent i = getIntent();
+        loginBy = i.getStringExtra("login");
+
+        btn_login.setOnClickListener(this);
+        btn_signup.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_signup) {
+            if (loginBy.equals("Dr")) {
+                Intent signUpIntent = new Intent(LoginActivity.this, SignUpActivityDr.class);
+                startActivity(signUpIntent);
+            }
+            if (loginBy.equals("patient")) {
+                Intent signUpIntent = new Intent(LoginActivity.this, SignUpActivityPatient.class);
+                startActivity(signUpIntent);
+            }
+        }
+        if(v.getId()== R.id.btn_login){
+            if (loginBy.equals("Dr")) {
+                Intent signUpIntent = new Intent(LoginActivity.this, Main3Activity.class);
+                signUpIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(signUpIntent);
+            }
+            if (loginBy.equals("patient")) {
+                Intent signUpIntent = new Intent(LoginActivity.this, Main2Activity.class);
+                signUpIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(signUpIntent);
+            }
+        }
     }
 }
--- app/src/main/java/com/main/hospital/hospitalinpocket/MainActivity.java
+++ app/src/main/java/com/main/hospital/hospitalinpocket/MainActivity.java
@@ -2,23 +2,17 @@ package com.main.hospital.hospitalinpocket;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 //import com.firebase.ui.auth.AuthUI;
 //import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnLoginPatient, btnLoginDr;
-//    FirebaseAuth fbAuth;
-//    FirebaseAuth.AuthStateListener authStateListener;
     public static final int SIGNINRQ=1;
 
     @Override
@@ -28,14 +22,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnLoginDr= (Button) findViewById(R.id.btnLoginDr);
         btnLoginPatient= (Button) findViewById(R.id.btnLoginPatient);
-
+        btnLoginDr.setOnClickListener(this);
+        btnLoginPatient.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
         if(view.getId()==R.id.btnLoginDr){
-//            signIn();
             Intent i=new Intent(MainActivity.this,LoginActivity.class);
             i.putExtra("login", "Dr");
             startActivity(i);
@@ -44,45 +38,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent i=new Intent(MainActivity.this,LoginActivity.class);
             i.putExtra("login","patient");
             startActivity(i);
-//            signIn();
         }
-    }
-
-   /* public void signIn(){
-        fbAuth = FirebaseAuth.getInstance();
-        authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if (firebaseAuth.getCurrentUser() != null) {
-
-                } else {
-                    Intent signInIntent = AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setIsSmartLockEnabled(false)
-                            .setTheme(R.style.Theme_Design)
-                            .setAvailableProviders(Arrays.asList(
-                                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
-                                    new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()
-                            ))
-                            .build();
-                    startActivityForResult(signInIntent, SIGNINRQ);
-                }
-            }
-        };
-    }*/
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == SIGNINRQ) {
-            if (resultCode == RESULT_OK) {
-//                Toast.makeText(MainActivity.this, "Welcome " + fbAuth.getCurrentUser().getDisplayName(), Toast.LENGTH_LONG).show();
-
-            } else {
-//                Toast.makeText(MainActivity.this, "Sign in failed", Toast.LENGTH_LONG).show();
-                finish();
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
     }
 }
